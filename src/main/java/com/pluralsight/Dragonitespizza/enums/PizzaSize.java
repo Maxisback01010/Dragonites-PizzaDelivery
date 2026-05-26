50c19afc22287bb765cd7eb488787c07aa8f560d
Max <marleyjodici@gmail.com>
2026-05-26 11:03:23 -0700

    Created enum class

--- src/main/java/com/pluralsight/Dragonitespizza/enums/PizzaSize.java
+++ src/main/java/com/pluralsight/Dragonitespizza/enums/PizzaSize.java
@@ -1,5 +1,7 @@
 package com.pluralsight.Dragonitespizza.enums;
 
+// Enums **short for enumerations** create a fixed set of named values
+// This helps prevent mistakes and restrict choices
 public class enum PizzaSize {
 
     PERSONAL,
