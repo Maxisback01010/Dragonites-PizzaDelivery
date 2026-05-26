d4661d5e960dc7271ef4b0da65d9665414e3930f
Max <marleyjodici@gmail.com>
2026-05-26 14:11:18 -0700

    Updated new fields

--- src/main/java/com/pluralsight/Dragonitespizza/models/Pizza.java
+++ src/main/java/com/pluralsight/Dragonitespizza/models/Pizza.java
@@ -1,6 +1,7 @@
 package com.pluralsight.Dragonitespizza.models;
 
-
+//Created pizza fields bc Pizza size will be different from drink size
+//Pizza handles it's own size bc of separate concepts
 import com.pluralsight.Dragonitespizza.enums.PizzaSize;
 
 // Pizza will automatically inherit Price and Size
