4fc8b05f0cd9685e24205c6a34a4de64dd5f7310
Max <marleyjodici@gmail.com>
2026-05-26 14:08:19 -0700

    Updated new fields

--- src/main/java/com/pluralsight/Dragonitespizza/enums/PizzaSize.java
+++ src/main/java/com/pluralsight/Dragonitespizza/enums/PizzaSize.java
@@ -1,8 +1,8 @@
 package com.pluralsight.Dragonitespizza.enums;
 
 // Enums **short for enumerations** create a fixed set of named values
-// This helps prevent mistakes and restrict choices
-public class enum PizzaSize {
+// This helps prevent mistakes/ bad data and restrict choices
+public enum PizzaSize {
 
     PERSONAL,
     MEDIUM,
--- src/main/java/com/pluralsight/Dragonitespizza/models/Pizza.java
+++ src/main/java/com/pluralsight/Dragonitespizza/models/Pizza.java
@@ -1,5 +1,9 @@
 package com.pluralsight.Dragonitespizza.models;
 
+
+import com.pluralsight.Dragonitespizza.enums.PizzaSize;
+
 // Pizza will automatically inherit Price and Size
 public class Pizza extends MenuItem {
+    private PizzaSize size;
 }
