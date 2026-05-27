40378bb87b10f0c426be1f02bf979f60f3ef1942
Max <marleyjodici@gmail.com>
2026-05-27 11:04:09 -0700

    Updated class and Created Arraylist

---
+++ src/main/java/com/pluralsight/Dragonitespizza/models/Order.java
@@ -0,0 +1,14 @@
+package com.pluralsight.Dragonitespizza.models;
+
+// This allows us to use the ArrayList tool
+
+import java.util.ArrayList;
+
+public class Order {
+ // Here I created an array list that knows that only pizza objects belong
+ // This will help prevent mistakes
+
+    private ArrayList<Pizza> pizzas;
+
+
+}
