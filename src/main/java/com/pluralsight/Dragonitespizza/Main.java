020cda005b12bb593d5f678faecade6e2aabed11
Max <marleyjodici@gmail.com>
2026-05-28 10:28:16 -0700

    Added constructor

--- src/main/java/com/pluralsight/Dragonitespizza/Main.java
+++ src/main/java/com/pluralsight/Dragonitespizza/Main.java
@@ -4,12 +4,15 @@ import com.pluralsight.Dragonitespizza.ui.HomeScreen;
 
 public class Main {
 
- //At this point in my code I
+ //At this point in my code I have made it so that things are deligated
+    //there are different functions for classes, methods, and responsibilities
 
     public static void main(String[] args) {
-
+//We orginally had main as a testing zone that created pizzas. It no longer does that
+// this creates the UI controller
         HomeScreen homeScreen = new HomeScreen();
-
+//This is hands controller to homescreen class
+ //Main is letting HomeScreen know to handle user interactions
         homeScreen.display();
     }
 }
