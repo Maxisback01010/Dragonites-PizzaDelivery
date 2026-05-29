2fbb52103d60ca4d4e9c6b4dcf862d90a3882e3a
Max <marleyjodici@gmail.com>
2026-05-28 17:15:59 -0700

    updated new switch

--- src/main/java/com/pluralsight/Dragonitespizza/enums/PizzaSize.java
+++ src/main/java/com/pluralsight/Dragonitespizza/enums/PizzaSize.java
@@ -8,4 +8,10 @@ public enum PizzaSize {
     PERSONAL,
     MEDIUM,
     LARGE
-}
+    }
+
+
+
+
+
+
--- src/main/java/com/pluralsight/Dragonitespizza/ui/OrderScreen.java
+++ src/main/java/com/pluralsight/Dragonitespizza/ui/OrderScreen.java
@@ -1,6 +1,8 @@
 package com.pluralsight.Dragonitespizza.ui;
 
+import com.pluralsight.Dragonitespizza.enums.PizzaSize;
 import com.pluralsight.Dragonitespizza.models.Order;
+
 import java.util.Scanner;
 
 
@@ -33,26 +35,73 @@ public OrderScreen(Order order){
 
         switch (choice){
 
+
             case 1:
-                System.out.println("Add pizza selected. ");
+
+                System.out.println("\nChoose Pizza Size");
+                System.out.println("1) Personal");
+                System.out.println("2) Medium");
+                System.out.println("3) Large");
+
+                int sizeChoice = scanner.nextInt();
+
+                // Variable declared BEFORE switch
+                PizzaSize size;
+
+                // Convert numeric input into enum value
+                switch (sizeChoice) {
+
+                    case 1:
+                        size = PizzaSize.PERSONAL;
+                        break;
+
+                    case 2:
+                        size = PizzaSize.MEDIUM;
+                        break;
+
+                    case 3:
+                        size = PizzaSize.LARGE;
+                        break;
+
+                    default:
+                        System.out.println("Invalid size.");
+                        return;
+                }
+
+                // Pass selected size into method
+                addPizza(size);
+
                 break;
 
             case 2:
-                System.out.println("Check out selected. ");
+                System.out.println("Checkout selected.");
                 break;
 
             case 0:
                 ordering = false;
-                System.out.println(" Order canceled. ");
+                System.out.println("Order canceled.");
                 break;
 
             default:
-                System.out.println("Invalid option. ");
+                System.out.println("Invalid option.");
 
         }
 
 
     }
+
  }
+  // Instead of putting a huge code into a switch statement I wanted to delegate responsibilties into methods
+  // My methods should be handling how it happens not what happens
+    private void addPizza(PizzaSize size) {
+        System.out.println("Pizza added with size: " + size);
+
+
+
+
+
+
+    }
+
 
 }
