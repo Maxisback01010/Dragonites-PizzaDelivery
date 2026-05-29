8c03f3ee190370d04f112cb283ea77177d40dacc
Max <marleyjodici@gmail.com>
2026-05-28 22:21:59 -0700

    updated enums

---
+++ src/main/java/com/pluralsight/Dragonitespizza/enums/Topping.java
@@ -0,0 +1,13 @@
+package com.pluralsight.Dragonitespizza.enums;
+
+public enum Topping {
+    PEPPERONI,
+    SAUSAGE,
+    BACON,
+    HAM,
+    MUSHROOMS,
+    ONIONS,
+    OLIVES,
+    PEPPERS,
+    EXTRA_CHEESE
+}
